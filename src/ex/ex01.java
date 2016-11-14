b663ea736080cfd793b6b163a9870862eb060950
EXsunshine <105021057@live.asia.edu.tw>
2016-11-14 16:14:42 +0800

    030

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -3,11 +3,19 @@ package ex;
  * Date: 2016/11/14
  * Author: 1050210XX OOO
  */
+import java.util.*;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn = new Scanner(System.in);
+int a = scn.nextInt();
+System.out.print(fun(a));
 	}
-
+	public static int fun(int a){
+		int[]data = new int[a];
+		
+		return a;
+		
+	}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,25 @@
 package hw;
-
+import java.util.*;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner scn = new Scanner(System.in);
+long a = scn.nextLong();
+System.out.println(fun(a));
 	}
-
+	public static Long fun(Long x){
+		Long sum = (long) 0;
+		
+			while(x>0){
+				sum++;
+				x = x/10;
+				
+			}
+			return sum;
+			
+			
+		}
+		
+	
 }
